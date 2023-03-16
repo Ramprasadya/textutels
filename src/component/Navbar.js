795468c65789cf424ae5ce0fe08cf12ac16d94be
Ramprasadya <yadavramprasad563@gmail.com>
2023-03-16 14:16:07 +0530

    Fix the problem

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -7,8 +7,8 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <Link className="navbar-brand" to="">
-          {props.title}
+        <Link className="navbar-brand" to="/">
+        Textutels
         </Link>
         <button
           className="navbar-toggler"
